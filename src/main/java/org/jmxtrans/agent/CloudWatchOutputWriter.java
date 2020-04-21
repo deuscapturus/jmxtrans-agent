e915173db9a3ef5a93fb5bbd6a2676984ab57440
Theodore Cowan <theodore-cowan@pluralsight.com>
2020-04-21 19:20:04 -0400

    (CloudwatchOutputWriter) a beginning

---
+++ src/main/java/org/jmxtrans/agent/CloudWatchOutputWriter.java
@@ -0,0 +1,15 @@
+package org.jmxtrans.agent;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cloudwatch.CloudWatchClient;
+import software.amazon.awssdk.services.cloudwatch.model.Dimension;
+import software.amazon.awssdk.services.cloudwatch.model.MetricDatum;
+import software.amazon.awssdk.services.cloudwatch.model.StandardUnit;
+import software.amazon.awssdk.services.cloudwatch.model.PutMetricDataRequest;
+import software.amazon.awssdk.services.cloudwatch.model.PutMetricDataResponse;
+import software.amazon.awssdk.services.cloudwatch.model.CloudWatchException;
+
+
+public class CloudWatchOutputWriter extends AbstractOutputWriter {
+
+}
