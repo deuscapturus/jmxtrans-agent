d3d88b9a2c932092368dc0a36bf64f88a59d6e76
Theodore Cowan <theodore-cowan@pluralsight.com>
2020-04-22 10:39:26 -0400

    work-in-progress. note that I do no know Java at all

--- src/main/java/org/jmxtrans/agent/CloudWatchOutputWriter.java
+++ src/main/java/org/jmxtrans/agent/CloudWatchOutputWriter.java
@@ -12,4 +12,54 @@ import software.amazon.awssdk.services.cloudwatch.model.CloudWatchException;
 
 public class CloudWatchOutputWriter extends AbstractOutputWriter {
 
+    CloudWatchClient client = CloudWatchClient.create();
+    private final String configNamespace = ConfigurationUtils.getString(settings, "namespace", "JMX");
+    private final String configDimensions = ConfigurationUtils.getString(settings, "dimensions", "");
+    /* Use use Tag class to extract dimensions.  I don't know if Tag is in the right scope to work here. */
+    private final List<Tag> listOfDimensions = Tag.tagsFromCommaSeparatedString(configDimensions);
+    private List<Dimension> dimensions = new ArrayList<>();
+
+    for (Tag thisDimension : listOfDimensions) {
+        dimensions.add(new Dimension().withName(thisDimension.getName()).withValue(thisDimension.getvalue()));
+    }
+
+    @Override
+    public void writeQueryResult(@Nonnull String name, @Nullable String type, @Nullable Object value) {
+        /* Do not use variable 'type'.  It is only set with a "invocation" (instead of a "query"), but the documentation only mentions "query" */
+
+        if (value instanceof Number) {
+            value = value.doubleValue()
+        } else {
+            return
+        }
+
+        try {
+            MetricDatum datum = MetricDatum.builder()
+                .metricName(name)
+                .unit(StandardUnit.NONE)
+                .value(value)
+                .dimensions(dimensions).build();
+
+            PutMetricDataRequest request = PutMetricDataRequest.builder()
+                .namespace(configNamespace)
+                .metricData(datum).build();
+
+            client.putMetricData(request);
+
+        } catch (CloudWatchException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+        }
+    }
+
+    @Override
+    public void writeInvocationResult(@Nonnull String invocationName, @Nullable Object value) throws IOException {
+        writeQueryResult(invocationName, null, value);
+    }
+
+    @Override
+    public void preDestroy() {
+        super.preDestroy();
+        client.close();
+    }
+
 }
