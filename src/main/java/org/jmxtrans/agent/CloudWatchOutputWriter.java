b71f7e2436e87f83cba35f8237e957cec2e27037
Theodore Cowan <theodore-cowan@pluralsight.com>
2020-04-22 22:42:33 -0400

    bundle all deps.  add logging.  Not working.  stalls at client.putMetricData

--- src/main/java/org/jmxtrans/agent/CloudWatchOutputWriter.java
+++ src/main/java/org/jmxtrans/agent/CloudWatchOutputWriter.java
@@ -7,13 +7,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.logging.Level;
 import java.io.IOException;
 
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cloudwatch.CloudWatchClient;
 import software.amazon.awssdk.services.cloudwatch.model.Dimension;
 import software.amazon.awssdk.services.cloudwatch.model.MetricDatum;
-import software.amazon.awssdk.services.cloudwatch.model.StandardUnit;
 import software.amazon.awssdk.services.cloudwatch.model.PutMetricDataRequest;
 import software.amazon.awssdk.services.cloudwatch.model.PutMetricDataResponse;
 import software.amazon.awssdk.services.cloudwatch.model.CloudWatchException;
@@ -29,32 +28,44 @@ public class CloudWatchOutputWriter extends AbstractOutputWriter {
 
     @Override
     public void postConstruct(Map<String, String> settings) {
-        client = CloudWatchClient.builder().build();
+
+        client = CloudWatchClient.create();
         configNamespace = ConfigurationUtils.getString(settings, "namespace", "JMX");
         configDimensions = ConfigurationUtils.getString(settings, "dimensions", "");
         /* Use use Tag class to extract dimensions.  I don't know if Tag is in the right scope to work here. */
         listOfDimensions = Tag.tagsFromCommaSeparatedString(configDimensions);
 
+        logger.log(getInfoLevel(), "listOfDimensions =" + configDimensions
+        + ", listOfDimensions=" + listOfDimensions);
+
+        /* FIXME
         for(Tag thisDimension : listOfDimensions) {
             dimensions.add(Dimension.builder().name(thisDimension.getName()).value(thisDimension.getValue()).build());
-        }
+        }*/
     }
 
     @Override
     public void writeQueryResult(String name, String type, Object value) {
         /* Do not use variable 'type'.  It is only set with a "invocation" (instead of a "query"), but the documentation only mentions "query" */
+
         Double doubleValue;
 
         if (value instanceof Number) {
-            doubleValue = (Double) value;
+            if (value instanceof Long) {
+                logger.log(Level.WARNING, "Cannot write result " + name + ". " + value + " because it is type Long.  TODO: Figure out how to case a java.lang.Object which is java.lang.Long as a Double.");
+                return;
+            } else {
+                doubleValue = (Double) value;
+            }
         } else {
+            logger.log(Level.WARNING, "Cannot write result " + name + ". " + value + " is not a Number.");
             return;
         }
 
         try {
+
             MetricDatum datum = MetricDatum.builder()
                 .metricName(name)
-                .unit(StandardUnit.NONE)
                 .value(doubleValue)
                 .dimensions(dimensions).build();
 
@@ -62,10 +73,12 @@ public class CloudWatchOutputWriter extends AbstractOutputWriter {
                 .namespace(configNamespace)
                 .metricData(datum).build();
 
-            client.putMetricData(request);
+            logger.log(Level.INFO, "request = " + request);
+            PutMetricDataResponse response = client.putMetricData(request);
+            logger.log(Level.INFO, "putMetricData response: " + response.toString());
 
         } catch (CloudWatchException e) {
-            System.err.println(e.awsErrorDetails().errorMessage());
+            logger.log(Level.SEVERE, e.awsErrorDetails().errorMessage());
         }
     }
 
